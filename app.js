f6d50494d5b238effa3645622a53c50e5970491e
Isaiah Ng <ngisaiah17@gmail.com>
2025-12-08 19:26:40 -0500

    Notes Functionality Added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const hbs = require('express-handlebars')
 const path = require('path')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 // Load config
@@ -35,6 +36,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGO_URI})
   })
 )
 
